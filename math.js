2061f1a5f8728d7db37290711d18d40877ef41a0
imrancoder10 <imranhossain1148@gmail.com>
2024-09-07 15:07:17 +0600

    Math.min/max/PI/abs/round/floor/ceil/random

---
+++ math.js
@@ -0,0 +1,18 @@
+const min = Math.min(45, 12, 16, 9, 9, 87);
+console.log(min);
+
+const max = Math.max(45, 12, 16, 9, 9, 87);
+console.log(max);
+
+console.log(Math.PI);
+
+console.log(Math.abs(8 - 88));
+console.log(Math.round(2.25));
+console.log(Math.round(2.58));
+console.log(Math.floor(2.98));
+console.log(Math.ceil(8.000001));
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const rand = Math.round(Math.random()*100);
+console.log(rand);
