2fb121863b5cd79ed9ce54ad5ce55b484230a2f3
imrancoder10 <imranhossain1148@gmail.com>
2024-09-07 22:20:34 +0600

    swap

---
+++ swap.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b= 7;
+
+console.log(a, b);
+
+const temp = 5;
+ a = b;
+ b = temp;
+ console.log(a, b);
+
+ //===============================================
+ let x = 5;
+ let y = 7;
+ [x, y] = [y, x];
+ console.log(x, y);
+
